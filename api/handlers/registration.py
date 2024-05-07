192d0e37c0b7c35a43fe6e70bbc973aae2dc7a67
u913g <131685404+u913g@users.noreply.github.com>
2024-05-07 19:41:34 +0100

    address is now encrypted in database

--- api/handlers/registration.py
+++ api/handlers/registration.py
@@ -1,5 +1,6 @@
 import os
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 from json import dumps
 from logging import info
@@ -70,7 +71,7 @@ class RegistrationHandler(BaseHandler):
         if user is not None:
             self.send_error(409, message='A user with the given email address already exists!')
             return
-
+########## Salt ######################
 
 #Step 1: generate the salt -  16 bytes (128 bits)
         salt = os.urandom(16)
@@ -84,13 +85,36 @@ class RegistrationHandler(BaseHandler):
         #cast salt into hex for displaying and checking
         saltH = salt.hex()
 
+########### Encryption ###############
+        key = "theluckykeyisace"
+    
+        key_bytes = bytes(key, "utf-8")
+        nonce_bytes = b'\x84\xf2\xc5]\x06\x1f\xc8Z\xb3\xe3\xc2\xd61\xf1~\xd4'
+
+    
+#aes in counter mode
+#nonce needed here in order to initialise it.
+#encryptor and decryptor created
+        aes_ctr_cipher = Cipher(algorithms.AES(key_bytes),
+                                mode=modes.CTR(nonce_bytes))
+        aes_ctr_encryptor = aes_ctr_cipher.encryptor()
+
+#user_address = user_address_in
+    #set plaintext
+        plaintext = address
+        plaintext_bytes = bytes(plaintext, "utf-8")
+
+    #encrypt plaintext
+        ciphertext_bytes = aes_ctr_encryptor.update(plaintext_bytes)
+        ciphertext = ciphertext_bytes.hex()
+
        
         ### Inserts the user data (including email, hashed password,
         ### encrypted address, etc.) into the database.
         yield self.db.users.insert_one({
             'email': email,
             'password': pwhashed,
-            'address': address,
+            'address': ciphertext,
             'dob': dob,
             'disability': disability,
             'salt': saltH,
@@ -100,7 +124,7 @@ class RegistrationHandler(BaseHandler):
         self.set_status(200)
         self.response['email'] = email
         self.response['password'] = pwhashed #added here for testing purposes
-        self.response['address'] = address
+        self.response['address'] = ciphertext
         self.response['dob'] = dob
         self.response['disability'] = disability
         self.response['salt'] = saltH # added it here for testing purposes
--- api/handlers/user.py
+++ api/handlers/user.py
@@ -1,4 +1,5 @@
 from tornado.web import authenticated
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 from .auth import AuthHandler
 
@@ -9,10 +10,29 @@ class UserHandler(AuthHandler):
         self.set_status(200)
         self.response['email'] = self.current_user['email']
         self.response['password'] = self.current_user['pwhashed']
-        self.response['address'] = self.current_user['address']
+
+        encrypted_address = self.current_user['address']
+        ciphertext = encrypted_address
+        ciphertext_bytes = bytes.fromhex(ciphertext)
+        
+        key = "theluckykeyisace"
+        key_bytes = bytes(key, "utf-8")
+        nonce_bytes = b'\x84\xf2\xc5]\x06\x1f\xc8Z\xb3\xe3\xc2\xd61\xf1~\xd4'
+
+#encryptor and decryptor created
+        aes_ctr_cipher = Cipher(algorithms.AES(key_bytes),
+                                mode=modes.CTR(nonce_bytes))
+        aes_ctr_decryptor = aes_ctr_cipher.decryptor()
+
+#decrypt ciphertext
+        plaintext_bytes = aes_ctr_decryptor.update(ciphertext_bytes)
+        plaintext = str(plaintext_bytes, "utf-8")
+
+        self.response['address'] = self.current_user['plaintext']
         self.response['dob'] = self.current_user['dob']
         self.response['disability'] = self.current_user['disability']
         self.response['salt'] = self.current_user['saltH']
         self.response['displayName'] = self.current_user['display_name']
+        
         self.write_json()
 
